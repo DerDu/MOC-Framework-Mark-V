7343802bcec256aad1695c4c4d55f962440f8284
Gerd Christian Kunze <gerdchristian.kunze@googlemail.com>
2014-11-18 15:13:45 +0100

    Update: Vendor Doctrine2 (DBAL 2.5.0 to ORM 2.5.0)

--- TestSuite/Tests/Component/Database/BridgeTest.php
+++ TestSuite/Tests/Component/Database/BridgeTest.php
@@ -2,6 +2,7 @@
 namespace MOC\V\TestSuite\Tests\Component\Database;
 
 use MOC\V\Component\Database\Component\Bridge\Repository\Doctrine2DBAL;
+use MOC\V\Component\Database\Component\Bridge\Repository\Doctrine2ORM;
 use MOC\V\Component\Database\Component\Exception\ComponentException;
 use MOC\V\Component\Database\Component\Parameter\Repository\DatabaseParameter;
 use MOC\V\Component\Database\Component\Parameter\Repository\DriverParameter;
@@ -22,6 +23,62 @@ class BridgeTest extends \PHPUnit_Framework_TestCase
     {
 
         $Bridge = new Doctrine2DBAL();
+
+        try {
+            $Bridge->registerConnection(
+                new UsernameParameter( '' ),
+                new PasswordParameter( '' ),
+                new DatabaseParameter( '' ),
+                new DriverParameter( DriverParameter::DRIVER_PDO_MYSQL ),
+                new HostParameter( null ),
+                new PortParameter( null )
+            );
+        } catch( \Exception $E ) {
+            $this->assertInstanceOf( 'MOC\V\Component\Database\Component\Exception\ComponentException', $E );
+        }
+
+        $Bridge->registerConnection(
+            new UsernameParameter( '' ),
+            new PasswordParameter( '' ),
+            new DatabaseParameter( '' ),
+            new DriverParameter( DriverParameter::DRIVER_PDO_SQLITE ),
+            new HostParameter( 'sqlite::memory:' ),
+            new PortParameter( null )
+        );
+
+        $this->assertInstanceOf( 'MOC\V\Component\Database\Component\IBridgeInterface',
+            $Bridge->prepareStatement( "SELECT * FROM UnitTest WHERE Id = ?" )
+        );
+        $this->assertInstanceOf( 'MOC\V\Component\Database\Component\IBridgeInterface',
+            $Bridge->defineParameter( 1 )
+        );
+        $this->assertInstanceOf( 'MOC\V\Component\Database\Component\IBridgeInterface',
+            $Bridge->prepareStatement( array( "SELECT * FROM UnitTest", "SELECT * FROM UnitTest" ) )
+        );
+        try {
+            try {
+                $Bridge->executeRead();
+            } catch( \Exception $E ) {
+                throw new ComponentException( $E->getMessage(), $E->getCode(), $E );
+            }
+        } catch( \Exception $E ) {
+            $this->assertInstanceOf( 'MOC\V\Component\Database\Component\Exception\ComponentException', $E );
+        }
+        try {
+            try {
+                $Bridge->executeWrite();
+            } catch( \Exception $E ) {
+                throw new ComponentException( $E->getMessage(), $E->getCode(), $E );
+            }
+        } catch( \Exception $E ) {
+            $this->assertInstanceOf( 'MOC\V\Component\Database\Component\Exception\ComponentException', $E );
+        }
+    }
+
+    public function testDoctrine2ORM()
+    {
+
+        $Bridge = new Doctrine2ORM();
 
         try {
             $Bridge->registerConnection(
