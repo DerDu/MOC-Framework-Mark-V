9a8e035fc4d54fc026eeb93b2ca328cdf8514458
Gerd Christian Kunze <gerdchristian.kunze@googlemail.com>
2014-11-18 15:17:58 +0100

    Update: Vendor Doctrine2 (DBAL 2.5.0 to ORM 2.5.0)

--- Component/Database/Component/Bridge/Repository/Doctrine2ORM.php
+++ Component/Database/Component/Bridge/Repository/Doctrine2ORM.php
@@ -1,19 +1,7 @@
 <?php
 namespace MOC\V\Component\Database\Component\Bridge\Repository;
 
-use Doctrine\DBAL\Connection;
-use Doctrine\DBAL\DriverManager;
-use Doctrine\DBAL\Statement;
-use MOC\V\Component\Database\Component\Bridge\Bridge;
-use MOC\V\Component\Database\Component\Exception\ComponentException;
-use MOC\V\Component\Database\Component\Exception\Repository\NoConnectionException;
 use MOC\V\Component\Database\Component\IBridgeInterface;
-use MOC\V\Component\Database\Component\Parameter\Repository\DatabaseParameter;
-use MOC\V\Component\Database\Component\Parameter\Repository\DriverParameter;
-use MOC\V\Component\Database\Component\Parameter\Repository\HostParameter;
-use MOC\V\Component\Database\Component\Parameter\Repository\PasswordParameter;
-use MOC\V\Component\Database\Component\Parameter\Repository\PortParameter;
-use MOC\V\Component\Database\Component\Parameter\Repository\UsernameParameter;
 use MOC\V\Core\AutoLoader\AutoLoader;
 
 /**
@@ -21,11 +9,8 @@ use MOC\V\Core\AutoLoader\AutoLoader;
  *
  * @package MOC\V\Component\Database\Component\Bridge
  */
-class Doctrine2ORM extends Bridge implements IBridgeInterface
+class Doctrine2ORM extends Doctrine2DBAL implements IBridgeInterface
 {
-
-    /** @var Connection[] $ConnectionList */
-    private static $ConnectionList = array();
 
     /**
      *
@@ -38,143 +23,4 @@ class Doctrine2ORM extends Bridge implements IBridgeInterface
             __DIR__.'/../../../Vendor/Doctrine2ORM/2.5.0/vendor/doctrine/common/lib' );
     }
 
-    /**
-     * @param UsernameParameter $Username
-     * @param PasswordParameter $Password
-     * @param DatabaseParameter $Database
-     * @param DriverParameter   $Driver
-     * @param HostParameter     $Host
-     * @param PortParameter     $Port
-     *
-     * @throws ComponentException
-     * @return IBridgeInterface
-     */
-    public function registerConnection(
-        UsernameParameter $Username,
-        PasswordParameter $Password,
-        DatabaseParameter $Database,
-        DriverParameter $Driver,
-        HostParameter $Host,
-        PortParameter $Port
-    ) {
-
-        try {
-            $Connection = DriverManager::getConnection( array(
-                'driver'   => $Driver->getDriver(),
-                'user'     => $Username->getUsername(),
-                'password' => $Password->getPassword(),
-                'host'     => $Host->getHost(),
-                'dbname'   => $Database->getDatabase(),
-                'port'     => $Port->getPort()
-            ) );
-        } catch
-        ( \Exception $E ) {
-            throw new ComponentException( $E->getMessage(), $E->getCode(), $E );
-        }
-
-        try {
-            $Connection->connect();
-        } catch( \Exception $E ) {
-            throw new ComponentException( $E->getMessage(), $E->getCode(), $E );
-        }
-
-        array_push( self::$ConnectionList, $Connection );
-        return $this;
-    }
-
-    /**
-     * @return array
-     * @throws \Doctrine\ORM\ORMException
-     */
-    public function executeRead()
-    {
-
-        $Query = $this->prepareQuery();
-        return $this->prepareConnection()->executeQuery( $Query[0], $Query[1], $Query[2] )->fetchAll();
-    }
-
-    /**
-     * @return array
-     */
-    private function prepareQuery()
-    {
-
-        /** @var Statement $Statement */
-        $Statement = array_pop( self::$StatementList );
-        $ParameterCount = substr_count( $Statement, '?' );
-        $QueryValue = array();
-        $QueryType = array();
-        for ($Run = 0; $Run < $ParameterCount; $Run++) {
-            $Parameter = array_pop( self::$ParameterList );
-            array_unshift( $QueryValue, $Parameter[0] );
-            array_unshift( $QueryType, $Parameter[1] );
-        }
-        return array( $Statement, $QueryValue, $QueryType );
-    }
-
-    /**
-     * @throws NoConnectionException
-     * @return Connection
-     */
-    private function prepareConnection()
-    {
-
-        $Index = count( self::$ConnectionList ) - 1;
-        if (!isset( self::$ConnectionList[$Index] )) {
-            // @codeCoverageIgnoreStart
-            throw new NoConnectionException( $Index );
-            // @codeCoverageIgnoreEnd
-        }
-        return self::$ConnectionList[$Index];
-    }
-
-    /**
-     * @return int
-     * @throws \Doctrine\ORM\ORMException
-     */
-    public function executeWrite()
-    {
-
-        $Query = $this->prepareQuery();
-        return $this->prepareConnection()->executeUpdate( $Query[0], $Query[1], $Query[2] );
-    }
-
-    /**
-     * WARNING: this may drop out with no replacement
-     *
-     * @return \Doctrine\DBAL\Schema\AbstractSchemaManager
-     * @throws NoConnectionException
-     * @codeCoverageIgnore
-     */
-    public function getSchemaManager()
-    {
-
-        return $this->prepareConnection()->getSchemaManager();
-    }
-
-    /**
-     * WARNING: this may drop out with no replacement
-     *
-     * @return Connection
-     * @throws NoConnectionException
-     * @codeCoverageIgnore
-     */
-    public function getConnection()
-    {
-
-        return $this->prepareConnection();
-    }
-
-    /**
-     * WARNING: this may drop out with no replacement
-     *
-     * @return QueryBuilder
-     * @throws NoConnectionException
-     * @codeCoverageIgnore
-     */
-    public function getQueryBuilder()
-    {
-
-        return $this->prepareConnection()->createQueryBuilder();
-    }
 }
