02a2b5289d3056263672c282dacbecbc4ed39403
Gerd Christian Kunze <gerdchristian.kunze@googlemail.com>
2015-01-20 15:26:10 +0100

    Update: TestSuite

--- TestSuite/Tests/Component/Document/ParameterTest.php
+++ TestSuite/Tests/Component/Document/ParameterTest.php
@@ -1,6 +1,8 @@
 <?php
 namespace MOC\V\TestSuite\Tests\Component\Document;
 
+use MOC\V\Component\Document\Component\Parameter\Repository\FileParameter;
+
 /**
  * Class ParameterTest
  *
@@ -19,4 +21,25 @@ class ParameterTest extends \PHPUnit_Framework_TestCase
         $this->assertInstanceOf( 'MOC\V\Component\Document\Component\Parameter\Parameter', $Parameter );
 
     }
+
+    public function testFileParameter()
+    {
+
+        try {
+            new FileParameter( null );
+        } catch( \Exception $E ) {
+            $this->assertInstanceOf( 'MOC\V\Component\Document\Component\Exception\Repository\EmptyFileException', $E );
+        }
+
+        $Parameter = new FileParameter( __FILE__ );
+        $this->assertEquals( __FILE__, $Parameter->getFile() );
+
+        try {
+            $Parameter->setFile( __DIR__ );
+        } catch( \Exception $E ) {
+            $this->assertInstanceOf( 'MOC\V\Component\Document\Component\Exception\Repository\TypeFileException', $E );
+        }
+
+    }
+
 }
