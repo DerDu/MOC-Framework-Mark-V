3068869b5a7770c6357b22aece2b294721986686
Gerd Christian Kunze <gerdchristian.kunze@googlemail.com>
2015-01-26 12:36:25 +0100

    Update: TestSuite

--- TestSuite/phpunit.php
+++ TestSuite/phpunit.php
@@ -8,3 +8,5 @@ require_once( __DIR__.'/../Core/AutoLoader/AutoLoader.php' );
 AutoLoader::getNamespaceAutoLoader( '\MOC\V', __DIR__.'/../' );
 
 set_include_path( get_include_path().PATH_SEPARATOR.__DIR__.'/../' );
+
+date_default_timezone_set( 'Europe/Berlin' );
