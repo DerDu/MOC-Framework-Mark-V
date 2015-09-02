3cdccd468e3f3d6dabce37e6401963184d5cb7df
Gerd Christian Kunze <gerdchristian.kunze@googlemail.com>
2015-09-02 09:11:19 +0200

    Update: TestSuite (SecureKernel)

--- Core/SecureKernel/Component/Bridge/Repository/SFTP.php
+++ Core/SecureKernel/Component/Bridge/Repository/SFTP.php
@@ -2,6 +2,8 @@
 namespace MOC\V\Core\SecureKernel\Component\Bridge\Repository;
 
 use MOC\V\Core\SecureKernel\Component\Bridge\Bridge;
+use MOC\V\Core\SecureKernel\Component\Bridge\Repository\SFTP\Directory;
+use MOC\V\Core\SecureKernel\Component\Bridge\Repository\SFTP\File;
 use MOC\V\Core\SecureKernel\Component\Exception\ComponentException;
 use MOC\V\Core\SecureKernel\Component\IBridgeInterface;
 
@@ -15,11 +17,100 @@ class SFTP extends Bridge implements IBridgeInterface
 
     /** @var null|\Net_SFTP */
     private $Connection = null;
+    /** @var string $Host */
+    private $Host = 'localhost';
+    /** @var int $Port */
+    private $Port = 22;
+    /** @var int $Timeout */
+    private $Timeout = 10;
+    /** @var string $Username */
+    private $Username = '';
+    /** @var null|string $Password */
+    private $Password = null;
+    /** @var null|string $Key */
+    private $Key = null;
 
     function __construct()
     {
 
         require_once( __DIR__.'/../../../Vendor/PhpSecLib/0.3.9/vendor/autoload.php' );
+    }
+
+    /**
+     * @return SFTP
+     */
+    public function closeConnection()
+    {
+
+        if ($this->Connection->isConnected()) {
+            $this->Connection->disconnect();
+        }
+        $this->Connection = null;
+        return $this;
+    }
+
+    /**
+     * @param string $Name
+     *
+     * @return bool
+     */
+    public function existsDirectory($Name)
+    {
+
+        $List = $this->listDirectory();
+        if (array_key_exists($Name, $List)) {
+            if ($List[$Name] instanceof Directory) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    /**
+     * @param null|string $Name
+     *
+     * @return SFTP\Directory[]|SFTP\File[]
+     */
+    public function listDirectory($Name = null)
+    {
+
+        $this->persistConnection();
+
+        if (null === $Name) {
+            $Name = $this->Connection->pwd();
+        }
+
+        $List = $this->Connection->rawlist($Name);
+        $Return = array();
+
+        foreach ($List as $Item => $Attributes) {
+            if ($this->Connection->is_dir($Item)) {
+                $Return[$Item] = new Directory($this, $Attributes);
+            }
+            if ($this->Connection->is_file($Item)) {
+                $Return[$Item] = new File($this, $Attributes);
+            }
+        }
+
+        return $Return;
+    }
+
+    /**
+     * @return SFTP
+     * @throws ComponentException
+     */
+    public function persistConnection()
+    {
+
+        if (!$this->Connection->isConnected()) {
+            $this->openConnection($this->Host, $this->Port, $this->Timeout);
+            if (null === $this->Key) {
+                $this->loginCredential($this->Username, $this->Password);
+            } else {
+                $this->loginCredentialKey($this->Username, $this->Key, $this->Password);
+            }
+        }
+        return $this;
     }
 
     /**
@@ -29,21 +120,13 @@ class SFTP extends Bridge implements IBridgeInterface
      *
      * @return SFTP
      */
-    public function openConnection( $Host, $Port = 22, $Timeout = 10 )
+    public function openConnection($Host, $Port = 22, $Timeout = 10)
     {
 
-        $this->Connection = new \Net_SFTP( $Host, $Port, $Timeout );
-        return $this;
-    }
-
-    /**
-     * @return SFTP
-     */
-    public function closeConnection()
-    {
-
-        $this->Connection->disconnect();
-        $this->Connection = null;
+        $this->Host = $Host;
+        $this->Port = $Port;
+        $this->Timeout = $Timeout;
+        $this->Connection = new \Net_SFTP($Host, $Port, $Timeout);
         return $this;
     }
 
@@ -54,16 +137,18 @@ class SFTP extends Bridge implements IBridgeInterface
      * @return SFTP
      * @throws ComponentException
      */
-    public function loginCredential( $Username, $Password = null )
+    public function loginCredential($Username, $Password = null)
     {
 
+        $this->Username = $Username;
+        $this->Password = $Password;
         if (null === $Password) {
-            if (!$this->Connection->login( $Username )) {
-                throw new ComponentException( 'Login failed' );
+            if (!$this->Connection->login($Username)) {
+                throw new ComponentException(__METHOD__.': Login failed');
             }
         } else {
-            if (!$this->Connection->login( $Username, $Password )) {
-                throw new ComponentException( 'Login failed' );
+            if (!$this->Connection->login($Username, $Password)) {
+                throw new ComponentException(__METHOD__.': Login failed');
             }
         }
         return $this;
@@ -77,34 +162,73 @@ class SFTP extends Bridge implements IBridgeInterface
      * @return SFTP
      * @throws ComponentException
      */
-    public function loginCredentialKey( $Username, $File, $Password = null )
+    public function loginCredentialKey($Username, $File, $Password = null)
     {
+
+        $this->Username = $Username;
+        $this->Key = $File;
+        $this->Password = $Password;
 
         $Key = new \Crypt_RSA();
         if (null !== $Password) {
-            $Key->setPassword( $Password );
+            $Key->setPassword($Password);
         }
-        if (!$Key->loadKey( file_get_contents( $File ) )) {
-            throw new ComponentException( 'Key failed' );
+        if (!$Key->loadKey(file_get_contents($File))) {
+            throw new ComponentException(__METHOD__.': Key failed');
         }
-        if (!$this->Connection->login( $Username, $Key )) {
-            throw new ComponentException( 'Login failed' );
+        if (!$this->Connection->login($Username, $Key)) {
+            throw new ComponentException(__METHOD__.': Login failed');
         }
         return $this;
     }
 
-    public function listDirectory()
+    /**
+     * @param string $Name
+     *
+     * @return bool
+     */
+    public function existsFile($Name)
     {
 
-        $List = $this->Connection->rawlist();
-        return $List;
+        $List = $this->listDirectory();
+        if (array_key_exists($Name, $List)) {
+            if ($List[$Name] instanceof File) {
+                return true;
+            }
+        }
+        return false;
     }
 
-    public function changeDirectory( $Directory )
+    /**
+     * @param string $Name
+     *
+     * @return SFTP
+     * @throws ComponentException
+     */
+    public function changeDirectory($Name)
     {
 
-        if (!$this->Connection->chdir( $Directory )) {
-            throw new ComponentException( 'Directory failed' );
+        $this->persistConnection();
+
+        if (!$this->Connection->chdir($Name)) {
+            throw new ComponentException(__METHOD__.': Failed');
+        }
+        return $this;
+    }
+
+    /**
+     * @param string $Name
+     *
+     * @return SFTP
+     * @throws ComponentException
+     */
+    public function createDirectory($Name)
+    {
+
+        $this->persistConnection();
+
+        if (!$this->Connection->mkdir($Name)) {
+            throw new ComponentException(__METHOD__.': Failed');
         }
         return $this;
     }
@@ -112,8 +236,10 @@ class SFTP extends Bridge implements IBridgeInterface
     public function uploadFile( $File )
     {
 
-        if (!$this->Connection->put( $File, file_get_contents( $File ) )) {
-            throw new ComponentException( 'Upload failed' );
+        $this->persistConnection();
+
+        if (!$this->Connection->put(basename($File), file_get_contents($File))) {
+            throw new ComponentException(__METHOD__.': Failed');
         }
         return $this;
     }
@@ -121,8 +247,10 @@ class SFTP extends Bridge implements IBridgeInterface
     public function downloadFile( $File )
     {
 
+        $this->persistConnection();
+
         if (!$this->Connection->get( $File, $File )) {
-            throw new ComponentException( 'Download failed' );
+            throw new ComponentException(__METHOD__.': Failed');
         }
         return $this;
     }
--- TestSuite/Tests/Core/SecureKernel/BridgeTest.php
+++ TestSuite/Tests/Core/SecureKernel/BridgeTest.php
@@ -11,26 +11,27 @@ use MOC\V\Core\SecureKernel\Component\Bridge\Repository\SFTP;
 class BridgeTest extends \PHPUnit_Framework_TestCase
 {
 
-    public function testUniversalRequest()
+    /**
+     * @
+     * @throws \MOC\V\Core\SecureKernel\Component\Exception\ComponentException
+     */
+    public function testSFTP()
     {
 
         $Bridge = new SFTP();
 
-        $Bridge->openConnection( '127.0.0.1', 22 );
-        $Bridge->loginCredentialKey( 'test', __DIR__.'/oasis-sftp-ssh2-rsa-4096-private.ppk', 'oasis' );
-        //$Bridge->loginCredential( 'test','test' );
-        var_dump( $Bridge->listDirectory() );
+        $Bridge->openConnection('host', 22);
+        $Bridge->loginCredentialKey('user', __DIR__.'/sftp-ssh2-rsa-4096-private.ppk', 'password');
+        $Bridge->changeDirectory('.');
         $Bridge->closeConnection();
+    }
 
-//        $this->assertInternalType( 'string', $Bridge->getPathBase() );
-//        $this->assertInternalType( 'string', $Bridge->getPathInfo() );
-//        $this->assertInternalType( 'string', $Bridge->getUrlBase() );
-//        $this->assertInternalType( 'string', $Bridge->getPort() );
-//
-//        $this->assertInternalType( 'array', $Bridge->getRequestGETArray() );
-//        $this->assertInternalType( 'array', $Bridge->getRequestPOSTArray() );
-//        $this->assertInternalType( 'array', $Bridge->getRequestCUSTOMArray() );
-//        $this->assertInternalType( 'array', $Bridge->getParameterArray() );
+    protected function setUp()
+    {
+
+        $this->markTestSkipped(
+            'SFTP not tested.'
+        );
     }
 
 }
