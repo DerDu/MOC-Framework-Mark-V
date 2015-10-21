68387afa09ee31fe1c372e276d135f45960f3e89
Gerd Christian Kunze <gerdchristian.kunze@googlemail.com>
2015-10-21 20:14:36 +0200

    Update: Core AutoLoader (Performance)

--- Core/AutoLoader/Vendor/Multiton/NamespaceLoader.php
+++ Core/AutoLoader/Vendor/Multiton/NamespaceLoader.php
@@ -19,6 +19,8 @@ class NamespaceLoader
     private $Extension = '.php';
     /** @var string $Prefix */
     private $Prefix = 'MOC\V';
+    /** @var string $Hash */
+    private $Hash = '';
 
     /**
      * @param string $Namespace
@@ -32,6 +34,24 @@ class NamespaceLoader
         $this->Path = $Path;
         if (null !== $Prefix) {
             $this->Prefix = $Prefix;
+        }
+        $this->Hash = $this->getLoaderHash();
+    }
+
+    /**
+     * @return string
+     */
+    public function getLoaderHash()
+    {
+
+        if (empty( $this->Hash )) {
+            return sha1(
+                serialize(
+                    get_object_vars($this)
+                )
+            );
+        } else {
+            return $this->Hash;
         }
     }
 
@@ -48,11 +68,11 @@ class NamespaceLoader
         }
 
         if (function_exists('apc_fetch')) {
-            $Hash = sha1($this->Namespace . $this->Path . $this->Separator . $this->Extension . $this->Prefix);
+            $Hash = sha1($this->Namespace.$this->Path.$this->Separator.$this->Extension.$this->Prefix);
             // @codeCoverageIgnoreStart
-            if (false === ($Result = apc_fetch($Hash . '#' . $ClassName))) {
+            if (false === ( $Result = apc_fetch($Hash.'#'.$ClassName) )) {
                 $Result = $this->checkCanLoadClass($ClassName);
-                apc_store($Hash . '#' . $ClassName, ($Result ? 1 : 0));
+                apc_store($Hash.'#'.$ClassName, ( $Result ? 1 : 0 ));
             }
             if (!$Result) {
                 return false;
@@ -65,17 +85,17 @@ class NamespaceLoader
         }
 
         /** @noinspection PhpIncludeInspection */
-        require($this->Path . DIRECTORY_SEPARATOR
-            . trim(str_replace(array($this->Prefix . $this->Separator, $this->Separator),
+        require( $this->Path.DIRECTORY_SEPARATOR
+            .trim(str_replace(array($this->Prefix.$this->Separator, $this->Separator),
                 array('', DIRECTORY_SEPARATOR), $ClassName), DIRECTORY_SEPARATOR)
-            . $this->Extension
+            .$this->Extension
         );
         return $this->checkExists($ClassName);
     }
 
     /**
      * @param string $Name
-     * @param bool $Load
+     * @param bool   $Load
      *
      * @return bool
      */
@@ -95,33 +115,20 @@ class NamespaceLoader
     public function checkCanLoadClass($ClassName)
     {
 
-        if ($this->Namespace !== null && strpos($ClassName, $this->Namespace . $this->Separator) !== 0) {
+        if ($this->Namespace !== null && strpos($ClassName, $this->Namespace.$this->Separator) !== 0) {
             return false;
         }
         $File = str_replace(
-                array($this->Prefix . $this->Separator, $this->Separator),
+                array($this->Prefix.$this->Separator, $this->Separator),
                 array('', DIRECTORY_SEPARATOR),
                 $ClassName
-            ) . $this->Extension;
+            ).$this->Extension;
         if ($this->Path !== null) {
-            return is_file($this->Path . DIRECTORY_SEPARATOR . $File);
+            return is_file($this->Path.DIRECTORY_SEPARATOR.$File);
         }
         // @codeCoverageIgnoreStart
         return false;
         // @codeCoverageIgnoreEnd
-    }
-
-    /**
-     * @return string
-     */
-    public function getLoaderHash()
-    {
-
-        return sha1(
-            serialize(
-                get_object_vars($this)
-            )
-        );
     }
 }
 
